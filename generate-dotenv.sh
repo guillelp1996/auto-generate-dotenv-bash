a21b375c6be27f13950e7be437886b5bee3df4f7
guillelp1996 <guillermovaldez8@gmail.com>
2023-02-22 21:54:36 +0100

    add local ip v4 variable

--- generate-dotenv.sh
+++ generate-dotenv.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+
+localip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 read -p "PORT: (default: '8080')" port
+clear
 read -p "HOST: (default: '127.0.0.1')" host
+clear
 read -p "DATABASE_HOST: (default: '127.0.0.1') " hostDB
+clear
 read -p "DATABASE_PORT: (default: '3306') " portDB
+clear
+read -p "LOCAL_IPv4: (default:'$localip')" localipCustom
+
 #TODO: change by switch case
 if [[ -z $port ]]; then
    port='8080'
@@ -19,15 +28,14 @@ fi
 if [[ -z $hostDB ]]; then
     hostDB='127.0.0.1'
 fi
+if [[ -z $localipCustom ]]; then
+    localipCustom=$localip
+fi 
 
-:'
-TODO:
-* add more variables
-* add selective path
-* add flags like -y, this flags can set defaults flags
-
-'
-
-output="PORT:$port\nHOST:'$host'\nDATABASE_PORT:$portDB\nDATABASE_HOST:'$hostDB'"
+output="PORT:$port\nHOST:'$host'\nDATABASE_PORT:$portDB\nDATABASE_HOST:'$hostDB'\nLOCAL_IPv4:'$localipCustom'"
 
 echo -e $output >> .env
+
+clear
+
+echo ".env created..."
