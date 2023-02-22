c93501454047ccc4ae8af0ecbeb6e5669b828c84
guillelp1996 <guillermovaldez8@gmail.com>
2023-02-22 23:03:45 +0100

    add variable 'DATABASE_USER'

--- generate-dotenv.sh
+++ generate-dotenv.sh
@@ -2,17 +2,33 @@
 
 
 localip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
 read -p "PORT: (default: '8080')" port
+
 clear
+
 read -p "HOST: (default: '127.0.0.1')" host
+
 clear
+
 read -p "DATABASE_HOST: (default: '127.0.0.1') " hostDB
+
 clear
+
 read -p "DATABASE_PORT: (default: '3306') " portDB
+
 clear
-read -p "LOCAL_IPv4: (default:'$localip')" localipCustom
+
+read -p "LOCAL_IPv4: (default: '$localip')" localipCustom
+
+clear
+
+read -p "DATABASE_USER: (default: 'root')" userDB
 
 #TODO: change by switch case
+if [[ -z $userDB ]]; then
+    userDB='root'
+fi
 if [[ -z $port ]]; then
    port='8080'
 fi
@@ -28,6 +44,7 @@ fi
 if [[ -z $hostDB ]]; then
     hostDB='127.0.0.1'
 fi
+
 if [[ -z $localipCustom ]]; then
     localipCustom=$localip
 fi 
@@ -38,4 +55,4 @@ echo -e $output >> .env
 
 clear
 
-echo ".env created..."
+echo ".env created . . ."
